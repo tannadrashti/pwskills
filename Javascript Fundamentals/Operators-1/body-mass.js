71e2ae8466f6f2ba199d0d14f45baeec6d8c5769
tannadrashti <145428360+tannadrashti@users.noreply.github.com>
2024-06-27 20:40:24 +0530

    Add files via upload

---
+++ Javascript Fundamentals/Operators-1/body-mass.js
@@ -0,0 +1,5 @@
+var w = 300;
+var h = 20;
+
+var bmi = w / h * h;
+console.log("BMI:" + bmi);
---
+++ Javascript Fundamentals/Operators-1/simple-interest.js
@@ -0,0 +1,6 @@
+var p = 100;
+var r = 5;
+var t = 2;
+
+var interest = (p * r * t) / 100;
+console.log("Simple Interest:" + interest);
