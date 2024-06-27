1b4094eb78b9e3f492b7c3982827d2ac5533bbc1
tannadrashti <145428360+tannadrashti@users.noreply.github.com>
2024-06-27 22:10:12 +0530

    Add files via upload

---
+++ Javascript -Fundamentals/Operators-1/area-of-circle.js
@@ -0,0 +1,6 @@
+const radius = 10;
+const pi = Math.PI;
+const areaofcircle = (radius, pi) => {
+    return pi * radius * radius;
+};
+console.log("The area of the circle is: " + areaofcircle(radius, pi));
